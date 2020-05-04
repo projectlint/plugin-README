238ccd536ae0130ec6eda9da4f3411e9a9e1043e
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2020-05-04 07:00:21 +0200

    Clean-up

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Plugin README
+
 README files plugin for projectlint, a style checker and lint tool for (Node.js) projects
 
 ## Contributing
--- __tests__/rules/standard readme.js
+++ __tests__/rules/standard readme.js
@@ -4,48 +4,15 @@ const { evaluate, fetch } = require("../../lib/rules/standard readme");
 
 const projectRoot = resolve(__dirname, "../..");
 
-test("evaluate", function() {
-  const result = evaluate({fetch: {result: { messages: [] }}});
+test("evaluate", function () {
+  const result = evaluate({ fetch: { result: { messages: [] } } });
 
-  expect(result).toBeFalsy();
+  expect(result).toMatchInlineSnapshot(`Array []`);
 });
 
-describe("fetch", function() {
-  test("default (exact)", function() {
-    return fetch({context: { projectRoot }}).then(function(result) {
-      expect(result).toMatchInlineSnapshot(`
-        VFile {
-          "contents": "# Plugin README
-
-        README files plugin for projectlint, a style checker and lint tool for (Node.js) projects
-
-        <https://www.npmjs.com/package/depcheck>
-
-        gitleaks
-
-        ## Contributing
-
-        <https://github.com/projectlint/plugin-README>
-
-        ## License
-
-        MIT
-        ",
-          "cwd": "${projectRoot}",
-          "data": Object {},
-          "history": Array [
-            "${projectRoot}/README.md",
-          ],
-          "messages": Array [
-            [${projectRoot}/README.md:1:1-1:16: Heading 'plugin readme' is not the directory name],
-          ],
-        }
-      `);
-    });
-  });
-
-  test("null option", function() {
-    return fetch({context: { projectRoot }, config: [null]}).then(function(result) {
+describe("fetch", function () {
+  test("default (exact)", function () {
+    return fetch({ context: { projectRoot } }).then(function (result) {
       expect(result).toMatchInlineSnapshot(`
         VFile {
           "contents": "# Plugin README
@@ -73,17 +40,46 @@ describe("fetch", function() {
     });
   });
 
-  test("slug", function() {
-    return fetch({context: { projectRoot }, config: ["slug"]}).then(function(result) {
+  test("null option", function () {
+    return fetch({ context: { projectRoot }, config: [null] }).then(function (
+      result
+    ) {
       expect(result).toMatchInlineSnapshot(`
         VFile {
           "contents": "# Plugin README
 
         README files plugin for projectlint, a style checker and lint tool for (Node.js) projects
 
-        <https://www.npmjs.com/package/depcheck>
+        ## Contributing
 
-        gitleaks
+        <https://github.com/projectlint/plugin-README>
+
+        ## License
+
+        MIT
+        ",
+          "cwd": "${projectRoot}",
+          "data": Object {},
+          "history": Array [
+            "${projectRoot}/README.md",
+          ],
+          "messages": Array [
+            [${projectRoot}/README.md:1:1-1:16: Heading 'plugin readme' is not the directory name],
+          ],
+        }
+      `);
+    });
+  });
+
+  test("slug", function () {
+    return fetch({ context: { projectRoot }, config: ["slug"] }).then(function (
+      result
+    ) {
+      expect(result).toMatchInlineSnapshot(`
+        VFile {
+          "contents": "# Plugin README
+
+        README files plugin for projectlint, a style checker and lint tool for (Node.js) projects
 
         ## Contributing
 
